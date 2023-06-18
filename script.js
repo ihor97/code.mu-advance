09410206ed75f5e564cbbb94626c3826e9a4e19b
ihor97 <daniel.waffner@gmail.com>
2023-06-18 20:08:10 +0100

    Единицы измерения в атрибуте style в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 /*
-Поменяем ему нужные стили:
+При указании значений свойств через атрибут style мы должны
+ указывать единицы измерения:
 
-Прочитаем его стили:
+ При необходимости мы можем избавиться 
+ от единиц измерения с помощью parseInt:
 
-*/ 
-elem.style.color = 'red';
-console.log(elem.style.color); // выведет 'red';
+ Если предполагаются дробные значения, 
+ можно воспользоваться функцией parseFloat:
+
+*/
+elem.style.width = '100px';
+let width = elem.style.width;
+console.log(width); // выведет '100px';
+
+{
+    let width = elem.style.width;
+    console.log(parseInt(width));
+}
+
+{
+    let width = elem.style.width;
+    console.log(parseFloat(width));
+}
