ef682d892c255bffd45c8888447896b6f29e1130
ihor97 <daniel.waffner@gmail.com>
2023-06-25 20:06:16 +0100

    Распахивание элемента в JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /*
-Прокрутим элемент до самого низа:
+Распахнем элемент на всю высоту:
+
 
 
 
 
 */
 let elem = document.querySelector('#elem');
-elem.scrollTop = elem.scrollHeight - elem.clientHeight;
+elem.style.height = elem.scrollHeight 
+	+ 'px'; 
+    elem.style.width = elem.scrollWidth 
+	+ 'px';
