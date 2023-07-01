523a114566b25427361b39df139c18f6fb0fecf0
ihor97 <daniel.waffner@gmail.com>
2023-07-01 19:09:34 +0100

    Перебор коллекций Map циклом

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ map.delete(arr1);
 Очистить всю коллекцию можно с помощью метода clear:
 map.clear();
 */ 
+let map = new Map;
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+map.set(arr1, 'data1');
+map.set(arr2, 'data2');
+
+// Можно отделить ключи и значения с помощью деструктуризации:
+
+for (let [key, elem] of map) {
+	console.log(key);
+	console.log(elem);
+}
