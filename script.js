0a36dce03488408e90f6653b1dcc1ebb5473f2bd
ihor97 <daniel.waffner@gmail.com>
2023-07-01 19:19:27 +0100

    Применение коллекций Map

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 /*
-Чтобы получить только ключи или только значения, существуют специальные методы. Для получения значений метод values:
-
-let values = map.values();
-Аналогично с ключами:
-
-let keys = map.keys();
-Существует также метод entries, возвращает набор пар ключ-значение:
-
-let entries = map.entries();
+Давайте сделаем так, чтобы по клику на каждый абзац ему в конец записывался 
+его порядковый номер в списке абзацев. Решим задачу через Map.
 */ 
+
+let map = new Map()
+
+let elems=document.querySelectorAll('p')
+let  i=1
+for (const elem of elems) {
+	map.set(elem,i)
+	i++
+}
+
+elems.forEach(e=>{
+	e.addEventListener('click',function (e) {
+		// цікаве діставання значення по обєкту
+		this.textContent+=map.get(this)
+	})
+})
