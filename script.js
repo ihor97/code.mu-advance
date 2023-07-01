c0f74608238a16e8585eff643473d3d94a19514d
ihor97 <daniel.waffner@gmail.com>
2023-07-01 19:08:14 +0100

    Полезные возможности коллекций Map

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 /*
-Коллекции Map представляют собой объекты, 
-в которых в качестве ключей могут выступать как примитивы, так и объекты.
+В свойстве size содержится размер коллекции:
+console.log(map.size);
+
+Проверить наличие ключа в коллекции можно с помощью метода has:
+console.log(map.has(arr1));
+
+Удалить элемент из коллекции можно с помощью метода delete:
+map.delete(arr1);
+
+Очистить всю коллекцию можно с помощью метода clear:
+map.clear();
 */ 
-let map = new Map;
-let arr1 = [1, 2];
-let arr2 = [3, 4];
-map.set(arr1, 'data1');
-map.set(arr2, 'data2');
-console.log(map.get(arr1));
-console.log(map.get(arr2));
